0b3409ca02e8ddb570aa4fbb8fa832d92801a8eb
CleissonVieira <cleisson.raimundi@gmail.com>
2022-06-21 09:21:38 -0300

    search new BottomSheet

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,14 +1,21 @@
+import 'react-native-gesture-handler';
+
 import AppLoading from 'expo-app-loading';
 
 import { View } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
-import { useFonts, Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
-
-
-import { theme } from './src/theme';
-import { Widget } from './src/components/Widget';
+import { 
+  useFonts, 
+  Inter_400Regular, 
+  Inter_500Medium } 
+  from '@expo-google-fonts/inter';
+  import * as SplashScreen from 'expo-splash-screen';
+  
+  import Widget from './src/components/Widget';
+  import { theme } from './src/theme';
 
 export default function App() {
+  SplashScreen.preventAutoHideAsync();
   const [fontsLoaded] = useFonts({
     Inter_400Regular,
     Inter_500Medium
@@ -18,18 +25,20 @@ export default function App() {
     return <AppLoading />;
   }
 
+  SplashScreen.hideAsync();
+
   return (
     <View style={{
       flex: 1,
       backgroundColor: theme.colors.background
     }}>
-      <Widget />
-
       <StatusBar 
         style='light'
         backgroundColor='transparent' 
         translucent
       />
+
+      <Widget />
     </View>
   );
 }
